a1c98acef82999ea78931dbc98139641f949d766
rdurel423 <rdurel423@gmail.com>
2014-03-14 19:55:51 -0500

    Revert "Update and Rename"
    This reverts commit 5a63cba9075721757e28e5e1852b0088f1a191b1.

--- StateMachineDemo/Assets/Scripts/FSM/Abstract/AbstractControl.cs
+++ StateMachineDemo/Assets/Scripts/FSM/Abstract/AbstractControl.cs
@@ -7,26 +7,11 @@ using System.Collections;
 /// methods of their machine.
 /// </summary>
 public abstract class AbstractControl : MonoBehaviour {
-    private AbstractStateMachine stateMachine;
-
-    /// <summary>
-    /// By default the control updates the state machine on each update. This ensures
-    /// that the client conforms to the contract of using a state machine in this system.
-    /// </summary>
-    void Update()
-    {
-        if (stateMachine == null)
-            throw new System.ArgumentException("State machine cannot be null", "original");
-
-        stateMachine.Update();
-    }
-
+    
     /// <summary>
     /// Depending on the type of GameObject, there might be updates required regardless of state.
     /// In this case, GlobalUpdate should be called to handle such update logic. This is also where
     /// transition-pertinent logic can be calculated.
-    /// 
-    /// This method
     /// </summary>
-    public virtual void GlobalUpdate() { } 
+    public virtual void GlobalUpdate() { }
 }
