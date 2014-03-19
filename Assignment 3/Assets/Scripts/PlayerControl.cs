9a5c15bc64a04e8f418f74e0b08a717fa45df9da
Ryan Durel <rdurel423@gmail.com>
2014-03-19 13:57:22 -0500

    Add Player
    Added a player controller and prefab. The player wraps around the screen
    when moving past its edge (wraps when overlapping Navigation bar).

---
+++ Assignment 3/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// All the player object needs to do is respond to movement.
+/// As such, there's no point in making it part of the state machine.
+/// </summary>
+public class PlayerControl : MonoBehaviour {
+    public float velocity;
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(x, y, 0.0f);
+        movement = movement * velocity;
+        
+        rigidbody.velocity = movement * velocity;
+
+        /* Distance from origin to top and bottom bounds
+         * is the same.
+         */
+        if (Mathf.Abs(transform.position.y) > Bounds.Y_MAX)
+        {
+            float newY = -transform.position.y;
+            transform.position = new Vector3(transform.position.x, newY, 0.0f);
+        }
+
+        if (transform.position.x > Bounds.X_MAX)
+        {
+            transform.position = new Vector3(Bounds.X_MIN, transform.position.y, 0.0f);
+        }
+        else if (transform.position.x < Bounds.X_MIN)
+        {
+            transform.position = new Vector3(Bounds.X_MAX, transform.position.y, 0.0f);
+        }
+    }
+}
---
+++ Assignment 3/Assets/Scripts/Utility/Bounds.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Represents the bounds of the screen. Used for wrapping
+/// the player.
+/// </summary>
+public class Bounds : MonoBehaviour {
+    public static float Y_MAX = 20.0f;
+    public static float Y_MIN = -20.0f;
+    public static float X_MAX = 25.0f;
+    public static float X_MIN = -42.0f;
+}
