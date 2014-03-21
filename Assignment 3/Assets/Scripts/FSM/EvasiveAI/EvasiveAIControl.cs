503c91a3ecd4f5bd5c6cf87ddc3cf4934840754c
Ryan Durel <rdurel423@gmail.com>
2014-03-21 11:34:24 -0500

    Add Flee Behavior
    Created Evasive Steering state machine, added flee state. Created
    Evasive AI prefab. Updated GUI prefab.

---
+++ Assignment 3/Assets/Scripts/FSM/EvasiveAI/EvasiveAIControl.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EvasiveAIControl : AbstractControl {
+    public float speed;
+
+    public enum States
+    {
+        EVADE,
+        FLEE
+    }
+
+    public override StateMachine CreateMachine()
+    {
+        StateMachine machine = new StateMachine();
+        machine.AddDefaultState(new FleeState((int)States.FLEE, this));
+        return machine;
+    }
+
+    public override void GlobalUpdate()
+    {
+        if (Mathf.Abs(transform.position.y) > Bounds.Y_MAX)
+        {
+            float newY = -transform.position.y;
+            transform.position = new Vector3(transform.position.x, newY, 0.0f);
+        }
+
+        if (transform.position.x > Bounds.X_MAX)
+        {
+            transform.position = new Vector3(Bounds.X_MIN, transform.position.y, 0.0f);
+        }
+        else if (transform.position.x < Bounds.X_MIN)
+        {
+            transform.position = new Vector3(Bounds.X_MAX, transform.position.y, 0.0f);
+        }
+    }
+
+    public Transform GetPlayerTransform()
+    {
+        return GameObject.FindGameObjectWithTag("Player").transform;
+    }
+}
